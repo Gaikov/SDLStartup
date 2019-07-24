ee7d4300777fb243b3ccb508c8a275db430f782e
CapricornGaikov <gaikov@inboix.ru>
2019-07-24 20:28:46 +0300

    multiple unit test files

--- unit/main.cpp
+++ unit/main.cpp
@@ -2,6 +2,6 @@
 // Created by Raman Haikou on 2019-07-24.
 //
 
-#define BOOST_TEST_MODULE "C++ Unit Tests for MyTangledLibrary"
+#define BOOST_TEST_MODULE "C++ Unit Tests SDLExample"
 #include <boost/test/included/unit_test.hpp>
 
